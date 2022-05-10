5ad48d43eb51c982722f21aba4d5310509401933
manuelalvarez93 <mrmanuelalvarez@gmail.com>
2022-05-10 10:20:57 -0300

    Aqui he tratado en el metodo addItemTo... intentado modificar el arreglo pero los metodos usados no han servido, tambien usando el Object.properties luego pasando writable: true como propiedad pero tampoco. Vamos a borrar todo esto e intentar algo diferente

--- src/store/indexReducer.js
+++ src/store/indexReducer.js
@@ -4,8 +4,10 @@ import { configure } from "@testing-library/react";
 
 const initialCartVisibState = [{ visible: true }];
 const initialProductState = { productsArray: [] };
-let testArray = [{ title: "Oranges", quantity: 3, price: 6, total: 18}, 
-{ title: "Potatoes", quantity: 7, price: 3, total: 21 }];
+let testArray = [
+  { title: "Oranges", quantity: 3, price: 6, total: 18 },
+  { title: "Potatoes", quantity: 7, price: 3, total: 21 },
+];
 
 const cartVisibility = createSlice({
   name: "cartRed",
@@ -21,15 +23,33 @@ const productSlice = createSlice({
   name: "prodRed",
   initialState: initialProductState,
   reducers: {
-    getItems(state){
-        state.productsArray = testArray
+    getItems(state) {
+      state.productsArray = testArray;
     },
     addItemToProductsArray(state, action) {
       //por action.payload llega un objeto con 4 atrib: title, price, description y quantity
-      console.log(action.payload);
-      testArray = [...testArray, action.payload]
-      console.log(testArray)
-      state.productsArray = testArray
+      let itemRepeats = false;
+      let index = 0
+      for (const obj of testArray) {
+          index++
+        if (action.payload.title === obj.title) {
+        let newObj = {
+            ...obj,
+            quantity: obj.quantity + action.payload.quantity
+        }
+        console.log(newObj)
+        testArray.splice(index, 1, newObj) //reemplazo en el indice-position del array (1er arg) 1 elemento-objeto (2do arg) por el newObj (3er arg)
+        console.log(testArray)
+        /* state.productsArray = testArray;
+        itemRepeats = true; */
+        break;
+        }
+      }
+
+      if (!itemRepeats) {
+        testArray = [...testArray, action.payload];
+        state.productsArray = testArray;
+      }
     },
   },
 });
